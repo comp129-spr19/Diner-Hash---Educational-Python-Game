c91e5c2952a198c265e49c2e7ec4771a75ebf89d
Mark Fraser <m_fraser3@u.pacific.edu>
2019-03-02 20:33:56 -0800

    Change Loading Screen Display Time to 10 Seconds
    resolves #112

--- src/loading_screen.py
+++ src/loading_screen.py
@@ -10,7 +10,7 @@ from time import (
 
 class LoadingScreen(object):
     # class constants
-    __DISPLAY_TIME__ = 1  # in seconds
+    __DISPLAY_TIME__ = 10  # in seconds
     __START_X__ = 0
     __START_Y__ = 0
     LOAD_PATH = '../imgs/loading_screens/'
