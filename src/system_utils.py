00a8596cbf42e8804432167f3bd535225f931407
Mark Fraser <m_fraser3@u.pacific.edu>
2019-03-04 19:27:46 -0800

    Add end condition; Add misc enhancements
    With this commit, a GameState enum is created to represent the different
    stages of the game.  This variable is used as a reference for the main
    run loop to decide whether or not to keep running.  A mock win screen
    has been created as a placeholder for another commit to actually
    implement it.
    
    Other technical debt has been resolved, ranging from eliminating some
    magic numbers to creating an app feedback messages file to extract magic
    strings.
    
    Technical debt created includes:
    - Adding the win screen to app.py required importing new modules.
    Perhaps refactor loading_screen.py to be a general module that allows
    for the game to switch between panes conveniently.

--- src/system_utils.py
+++ src/system_utils.py
@@ -1,2 +1,17 @@
+# variables allowed to be accessed
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 800
+
+game_state = None
+
+
+# classes for system variables
+from enum import (
+    Enum
+)
+
+class GameState(Enum):
+    LOADING = 1
+    MAIN_LOOP = 2
+    WON = 3
+    QUITTING = 4
